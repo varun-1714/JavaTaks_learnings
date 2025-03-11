e84e8e6f6d138648e19953f55742441b47fc677f
P.varun kumar <85186666+varun-1714@users.noreply.github.com>
2025-03-12 00:07:18 +0530

    Create NonStaticvar
    Non static variables in java accessing using objects creation and calling it with reference variable of class

---
+++ NonStaticvar.java
@@ -0,0 +1,25 @@
+public class NonStaticvar {
+    byte b ;
+    short s ;
+    int i ;
+    long l1;
+    float f1;
+    double d1;
+    char ch;
+    boolean bool;
+    String str;
+    public   void main(String[] args) {
+
+    nonstatic e = new nonstatic();
+         System.out.println( e.b);
+         System.out.println( e.i);
+         System.out.println( e.l1);
+         System.out.println( e.f1);
+         System.out.println( e.d1);
+         System.out.println( e.ch);
+         System.out.println( e.bool);
+         System.out.println( e.str);
+ 
+     }
+ }
+  
